10d0f4fb71563182310f13fa912153381d8c0f4d
mertalev <101130780+mertalev@users.noreply.github.com>
2026-03-09 17:28:46 -0500

    dart simplifications

--- mobile/lib/utils/bootstrap.dart
+++ mobile/lib/utils/bootstrap.dart
@@ -22,7 +22,6 @@ import 'package:immich_mobile/infrastructure/repositories/db.repository.dart';
 import 'package:immich_mobile/infrastructure/repositories/log.repository.dart';
 import 'package:immich_mobile/infrastructure/repositories/logger_db.repository.dart';
 import 'package:immich_mobile/infrastructure/repositories/network.repository.dart';
-import 'package:immich_mobile/services/api.service.dart';
 import 'package:immich_mobile/infrastructure/repositories/store.repository.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
@@ -110,9 +109,5 @@ abstract final class Bootstrap {
     );
 
     await NetworkRepository.init();
-    await NetworkRepository.setHeaders(
-      ApiService.getRequestHeaders(),
-      ApiService.getServerUrls(),
-    );
   }
 }
